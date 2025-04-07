895c514807c6f5d3427e9a49680110c5a15d5f3b
Maksym Peshko <makspeshko@gmail.com>
2025-04-07 22:27:42 +0300

    Lesson 20

---
+++ cypress/pageObjects/ExpensesTab.js
@@ -0,0 +1,33 @@
+class ExpensesPage {
+    mainPageElements = {
+        modelsDropdown: () => cy.get('#carSelectDropdown').click(),
+        modelsOptions: () => cy.get('li.car-select-dropdown_item').contains('BMW 3'),
+        createExpense: () => cy.get('div > div:first-child > div > button').contains('Add an expense')
+    };
+    createModalElements = {
+        vehicleList: () => cy.get('select[name="carId"]').select('Audi TT'),
+        reportDate: () => cy.get('#addExpenseDate'),
+        mileageField: () => cy.get('#addExpenseMileage'),
+        litersField: () => cy.get('#addExpenseLiters'),
+        totalCostField: () => cy.get('#addExpenseTotalCost'),
+        addButton: ()=> cy.get('.modal-content').find('button.btn.btn-primary').contains('Add')
+    }
+    selectVehicle (){
+        this.mainPageElements.modelsDropdown();
+        this.mainPageElements.modelsOptions().click();
+    }
+    clickOnCreate (){
+        this.mainPageElements.createExpense().click();
+    }
+    fillInForm (){
+        this.createModalElements.vehicleList();
+        this.createModalElements.reportDate().clear();
+        this.createModalElements.mileageField().clear();
+        this.createModalElements.reportDate().type('07.04.2025');
+        this.createModalElements.mileageField().type('2500');
+        this.createModalElements.litersField().type('5');
+        this.createModalElements.totalCostField().type('450');
+        this.createModalElements.addButton().click({force:true});
+    }
+}
+export default new ExpensesPage();
---
+++ cypress/pageObjects/GarageTab.js
@@ -0,0 +1,49 @@
+class GaragePage {
+    addCarButton = {
+      addButton: () => cy.contains('button', 'Add car')
+    };
+  
+    brandCarList = {
+      brandAudi: () => cy.get('#addCarBrand').select('Audi'),
+      brandBMW: () => cy.get('#addCarBrand').select('BMW')
+    };
+  
+    audiModelList = {
+      audiTT: () => cy.get('#addCarModel option').select('TT'),
+      audiR8: () => cy.get('#addCarModel option').select('R8'),
+      audiQ7: () => cy.get('#addCarModel option').select('Q7'),
+      audiA6: () => cy.get('#addCarModel option').select('A6'),
+      audiA8: () => cy.get('#addCarModel option').select('A8')
+    };
+  
+    bmwModelList = {
+      bmwSeries3: () => cy.get('#addCarModel option').select('3'),
+      bmwSeries5: () => cy.get('#addCarModel').select('5'),
+      bmwX5: () => cy.get('#addCarModel option').select('X5'),
+      bmwX6: () => cy.get('#addCarModel option').select('X6'),
+      bmwZ3: () => cy.get('#addCarModel option').select('Z3')
+    };
+
+    createModalButtons = {
+        addButton: () => cy.get('.modal-footer').find('button.btn.btn-primary').contains('Add')
+    }
+  
+    openCreateCarForm() {
+      this.addCarButton.addButton().click();
+    }
+  
+    selectBmwBrand() {
+      this.brandCarList.brandBMW();
+    }
+    selectBmwSeries5Model(){
+        this.bmwModelList.bmwSeries5();
+    }
+    enterMileage(){
+        cy.get('#addCarMileage').type(150000);
+    }
+    saveNewVehicle(){
+        this.createModalButtons.addButton.click({ force: true });
+    }
+  }
+  
+  export default new GaragePage();
